2a9fe77645dab555800683d5703fbfb66ee4f006
Hugo Caille <hugo.caille@isen-lille.fr>
2015-11-10 00:43:01 +0100

    pushUserNext job and Follow afterSave hook

--- cloud/main.js
+++ cloud/main.js
@@ -44,30 +44,75 @@ Parse.Cloud.define('getNextEpisode', function(request, reply) {
     }.bind(this));
     if (result) {
       result.seriesId = request.params.seriesId;
+      result.seriesName = request.params.seriesName;
     }
     else {
-      result = {seriesId: request.params.seriesId}
+      result = {
+        seriesId: request.params.seriesId,
+        seriesName: request.params.seriesName
+      }
     }
     return reply.success(result);
   });
 });
 
 Parse.Cloud.define('userNextEpisodes', function(request, reply) {
+  var user = request.user;
+  if (request.params.userId) {
+    user = Parse.User.createWithoutData(request.params.userId);
+  }
   var query = new Parse.Query('Favorite');
-  query.equalTo('user', request.user);
+  query.equalTo('user', user);
   query.exists('tmdb_series_id');
   query.find({
     success: function(favorites) {
       var queries = [];
 
       favorites.forEach(function(favorite) {
-        queries.push(Parse.Cloud.run('getNextEpisode', {seriesId: favorite.get('tmdb_series_id')}));
+        queries.push(Parse.Cloud.run('getNextEpisode', {seriesId: favorite.get('tmdb_series_id'), seriesName: favorite.get('title')}));
       }.bind(this));
 
       Parse.Promise.when(queries).then(function() {
         var args = Array.prototype.slice.call(arguments);
         reply.success(args);
       });
+    }
+  });
+});
+
+Parse.Cloud.job("pushUserNext", function(request, status) {
+  var query = new Parse.Query(Parse.User);
+  query.each(function(user) {
+    var pushQuery = new Parse.Query(Parse.Installation);
+    pushQuery.equalTo('user', user);
+    Parse.Cloud.run('userNextEpisodes', {userId: user.id}).then(function(result) {
+      result.forEach(function(item) {
+        if (item.air_date) {
+          Parse.Push.send({
+            where: pushQuery,
+            data: {
+              title: item.seriesName + ' is going to air',
+              alert: item.seriesName + ' airs on ' + item.air_date,
+              uri: 'kubrick://media/tv/' + item.seriesId
+            }
+          });
+        }
+      });
+    });
+  });
+});
+
+Parse.Cloud.afterSave('Follow', function(request) {
+  var username = request.user.getUsername();
+  var other_user = request.object.get('other_user');
+  var pushQuery = new Parse.Query(Parse.Installation);
+  pushQuery.equalTo('user', other_user);
+  Parse.Push.send({
+    where: pushQuery,
+    data: {
+      title: username + ' followed you',
+      alert: 'Discover his favorites movies and series on Kubrick',
+      uri: 'kubrick://profile/' + request.user.id
     }
   });
 });
